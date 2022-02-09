1dc34601af60c605f01237c3f3498730ee3c48d0
sudharsan-selvaraj <sudharsanselvaraj.c@gmail.com>
2022-02-10 00:40:42 +0530

    feat:stream live device video for each session

--- web/src/components/UI/organisms/session-video.tsx
+++ web/src/components/UI/organisms/session-video.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Session from "../../../interfaces/session";
 import CommonUtils from "../../../utils/common-utils";
 import VideoPlayer from "../atoms/video-player";
@@ -29,6 +29,59 @@ type PropsType = {
   height: number;
 };
 
+const LiveVideoContainer = styled.div<{ height: number }>`
+  height: ${(props) => props.height}px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const VideoImg = styled.img<{ height: number }>`
+  height: ${(props) => props.height - 18}px;
+  widht: 100%;
+  object-fit: scale-down;
+  top: 10px;
+  left: 10px;
+`;
+
+const LiveBadge = styled.span`
+  color: red;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-weight: 500;
+
+  @keyframes blink {
+    20% {
+      opacity: 0.8;
+    }
+    40% {
+      opacity: 0.4;
+    }
+    60% {
+      opacity: 0;
+    }
+    80% {
+      opacity: 0.4;
+    }
+    100% {
+      opacity: 0.8;
+    }
+  }
+
+  &::before {
+    content: "";
+    display: inline-block;
+    background: red;
+    margin-right: 5px;
+    height: 8px;
+    width: 8px;
+    border-radius: 50%;
+    animation: blink 1s step-start 0s infinite;
+  }
+`;
+
 const getVideoNotFoundMessage = (session: Session) => {
   if (session.is_completed) {
     return "Error occured while saving the video";
@@ -39,6 +92,27 @@ const getVideoNotFoundMessage = (session: Session) => {
 
 export default function SessionVideo(props: PropsType) {
   const { session, height } = props;
+
+  const getStreamingVideo = () => {
+    if (!session.is_completed && !!session.live_stream_port) {
+      return (
+        <LiveVideoContainer height={height}>
+          <VideoImg
+            src={CommonUtils.getLiveVideoForSession(session.session_id)}
+            height={height}
+          />
+          <LiveBadge>LIVE</LiveBadge>
+        </LiveVideoContainer>
+      );
+    } else {
+      return (
+        <EmptyVideoContainer height={height}>
+          <EmptyMessage>{getVideoNotFoundMessage(session)}</EmptyMessage>
+        </EmptyVideoContainer>
+      );
+    }
+  };
+
   return (
     <Container>
       {session.video_path ? (
@@ -47,9 +121,7 @@ export default function SessionVideo(props: PropsType) {
           height={height}
         />
       ) : (
-        <EmptyVideoContainer height={height}>
-          <EmptyMessage>{getVideoNotFoundMessage(session)}</EmptyMessage>
-        </EmptyVideoContainer>
+        getStreamingVideo()
       )}
     </Container>
   );
